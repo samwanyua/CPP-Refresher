ec1543cc72c07ed512dcc3b2e5b4ec9133223c1f
samwanyua <samexample8@gmail.com>
2024-03-01 14:35:04 +0300

    Introduction notes

---
+++ intro.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <string> //for processing strings
+
+using namespace std; //standard
+
+int main(){
+    int age = 23;
+    cout << "I love c++" << endl;
+
+    return 0;
+}
+/*
+C++ is not pure OOP - we can code in c++ as one can code in C
+It is not a requirement to write everything in OOP
+ 
+C++ started being developed in 1979, first released in 1983
+
+Is a statically typed language, free form(no form of writing code, spaces don't really matter)
+
+It has faster execution - it has no overheads
+*/
