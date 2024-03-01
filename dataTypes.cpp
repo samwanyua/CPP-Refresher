744621b1627958c9ec0e943744feb76e4ff8e44b
samwanyua <samexample8@gmail.com>
2024-03-01 14:50:21 +0300

    Data types and variables

---
+++ dataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// using namespace std;
+
+int main(){
+    /* Data types */
+    char _initial = 'S';
+    bool isMale = true;
+    int age = 23;
+    float dataAvg = 2323.32; //represents a 32-bit floating-point number. It provides about 7 decimal digits of precision.
+    double mean = 234.23; //epresents a 64-bit floating-point number. It provides about 15 decimal digits of precision
+
+    std::string name = "string";
+    
+    std::cout << _initial << std::endl;
+
+    //constant variable
+    int const finalMarks = 78;
+
+
+
+
+    return 0;
+}
